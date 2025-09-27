5aa97d8384c4515d17f10993334bd5b994a27766
Arthur Bruno Cesar <arthurbrunocesar2005@hotmail.com>
2025-09-27 13:48:16 -0300

    Fix: Usar arquivos .php para CSS/JS - forçar MIME types corretos

--- public/css/app.css.php
+++ public/css/app.css.php
@@ -1,14 +1,14 @@
 <?php
-// Forçar tipo MIME correto para CSS
-header('Content-Type: text/css');
+header('Content-Type: text/css; charset=utf-8');
 header('Cache-Control: public, max-age=31536000');
+header('Expires: ' . gmdate('D, d M Y H:i:s', time() + 31536000) . ' GMT');
+header('X-Content-Type-Options: nosniff');
 
-// Ler e servir o arquivo CSS original
-$cssFile = __DIR__ . '/app.css';
-if (file_exists($cssFile)) {
-    readfile($cssFile);
+$file = __DIR__ . '/app.css';
+if (file_exists($file)) {
+    readfile($file);
 } else {
     http_response_code(404);
-    echo "/* CSS file not found */";
+    echo '/* CSS file not found */';
 }
-exit;
+?>
--- public/js/app.js.php
+++ public/js/app.js.php
@@ -1,14 +1,14 @@
 <?php
-// Forçar tipo MIME correto para JavaScript
-header('Content-Type: application/javascript');
+header('Content-Type: application/javascript; charset=utf-8');
 header('Cache-Control: public, max-age=31536000');
+header('Expires: ' . gmdate('D, d M Y H:i:s', time() + 31536000) . ' GMT');
+header('X-Content-Type-Options: nosniff');
 
-// Ler e servir o arquivo JS original
-$jsFile = __DIR__ . '/app.js';
-if (file_exists($jsFile)) {
-    readfile($jsFile);
+$file = __DIR__ . '/app.js';
+if (file_exists($file)) {
+    readfile($file);
 } else {
     http_response_code(404);
-    echo "/* JS file not found */";
+    echo '/* JS file not found */';
 }
-exit;
+?>
